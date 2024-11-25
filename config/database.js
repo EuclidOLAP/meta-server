e4b5ef0f60ef7c121b2fde911cd72d05d66763ef
Fir <czg@euclidolap.com>
2024-11-25 22:26:35 +0000

    ^ config/database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,17 @@
 const { Sequelize } = require('sequelize');
 
+const DB_HOST = 'dev.vm';
+const DB_USER = 'xiaobai';
+const DB_PASSWORD = '122333';
+const DB_NAME = 'olap_mddm_meta';
+// const DB_PORT = '3306';
+const DB_DIALECT ='mysql';
+
 // 连接到 MySQL 数据库
-const sequelize_conn = new Sequelize('olap_mddm_meta', 'xiaobai', '122333', {
-  host: 'dev.vm',
-  dialect: 'mysql',
-  logging: (sqlstr) => console.log(`\nSQL >>>>>> ${sqlstr}\n`)
+const sequelize_conn = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  logging: (sqlstr) => console.log(`\nSQL >>>>>>\n${sqlstr}\n<<<<<< sql ending >>>>>>`)
 });
 
 // 导出 sequelize_conn 实例
