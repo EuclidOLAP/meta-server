537035ae379b8427f1468b0d576a176a16e9ea2a
Fir <czg@euclidolap.com>
2024-12-19 00:21:31 +0000

    feat: add gRPC client for connect to olap-core service
    - Added `proto/euclidolap.proto` for gRPC definitions.
    - Implemented gRPC client in `src/grpc-client.js` and `src/grpc-client-impl.js`.
    - Updated `src/routes/md-query-api.js` to integrate gRPC client.
    - Modified `package.json` to include necessary dependencies for gRPC support.

---
+++ src/grpc-client.js
@@ -0,0 +1,24 @@
+const path = require('path'); // 用于构造路径
+const protoLoader = require('@grpc/proto-loader'); // 加载 .proto 文件
+const grpc = require('@grpc/grpc-js'); // gRPC 客户端库
+
+// 定义 .proto 文件的路径
+const PROTO_PATH = path.resolve(__dirname, '../proto/euclidolap.proto');
+
+// 使用 protoLoader 加载 .proto 文件
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,          // 保留字段大小写
+    longs: String,           // 将 int64 转换为字符串
+    enums: String,           // 将枚举类型转换为字符串
+    defaults: true,          // 包含默认值
+    oneofs: true,            // 包含 oneof 字段
+});
+
+// 加载 gRPC 服务定义
+const olapProto = grpc.loadPackageDefinition(packageDefinition).euclidolap;
+
+console.log('Loaded proto >>>>>>>>>>>>>>>>>>>:::::::::::::', olapProto);
+
+module.exports = {
+    olapProto,
+};
