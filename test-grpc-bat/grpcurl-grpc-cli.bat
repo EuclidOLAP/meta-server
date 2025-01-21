e2c8617625c20f318e81079311caae6f83ecb0f7
Fir <czg@euclidolap.com>
2025-01-21 09:09:50 +0000

    Implemented gRPC service for querying Cube instances by GID and name in src/index.js. Added additional test cases in test-grpc-bat/grpcurl-grpc-cli.bat.

--- test-grpc-bat/grpcurl-grpc-cli.bat
+++ test-grpc-bat/grpcurl-grpc-cli.bat
@@ -5,6 +5,18 @@ SET server=dev.vm:50051
 SET proto_path=D:\_temp
 SET proto_file=olapmeta.proto
 
-grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"gid\": 8766}" %server% olapmeta.OlapMetaService/GetCubeByGid
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"gid\": 500000000000001}" %server% olapmeta.OlapMetaService/GetCubeByGid
+
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"gid\": 500000000000002}" %server% olapmeta.OlapMetaService/GetCubeByGid
+
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"gid\": 500000000000003}" %server% olapmeta.OlapMetaService/GetCubeByGid
+
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"gid\": 500000000099999}" %server% olapmeta.OlapMetaService/GetCubeByGid
+
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"name\": \"CCC\"}" %server% olapmeta.OlapMetaService/GetCubeByName
+
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"name\": \"C2\"}" %server% olapmeta.OlapMetaService/GetCubeByName
+
+grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"name\": \"ccvv\"}" %server% olapmeta.OlapMetaService/GetCubeByName
 
 grpcurl --plaintext --import-path "%proto_path%" --proto "%proto_file%" -d "{\"name\": \"Nile River Store\"}" %server% olapmeta.OlapMetaService/GetCubeByName
