6d0120bde81769f3a96cdf0ed70473162df44708
Fir <czg@euclidolap.com>
2025-08-13 22:58:57 +0000

    Changes to be committed: 	new file:   models/readme_readme_readme_readme_readme_readme.txt 	new file:   src/models/AdhocQuery.js 	new file:   src/models/CalculatedMetric.js 	new file:   src/models/Cube.js 	new file:   src/models/Dashboard.js 	new file:   src/models/Dimension.js 	new file:   src/models/DimensionRole.js 	new file:   src/models/Hierarchy.js 	new file:   src/models/Level.js 	new file:   src/models/MdxExecutionLog.js 	new file:   src/models/Member.js

---
+++ src/models/AdhocQuery.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require("sequelize");
+const sequelize_conn = require("../config/database");
+
+// 定义 AdhocQuery 模型
+const AdhocQuery = sequelize_conn.define(
+  "AdhocQuery",
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    uuid: {
+      type: DataTypes.STRING(255),
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    created_by: {
+      type: DataTypes.BIGINT.UNSIGNED,
+    },
+    updated_by: {
+      type: DataTypes.BIGINT.UNSIGNED,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    cubeGid: {
+      type: DataTypes.BIGINT.UNSIGNED,
+    },
+    jsonStr: {
+      type: DataTypes.TEXT,
+    },
+    raw_data_table_json_str: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    tableName: "adhoc_query", // 指定数据库中的表名
+    freezeTableName: true, // 禁用 Sequelize 自动管理表结构
+    timestamps: true,
+    underscored: true, // 启用驼峰到下划线的自动转换
+  }
+);
+
+module.exports = AdhocQuery;
