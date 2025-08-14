47102c0c0e270c7a3a6e17adb76694a2eee82f60
Fir <czg@euclidolap.com>
2025-08-14 11:57:12 +0000

    Changes to be committed: 	modified:   index.tsx 	modified:   package.json

--- index.tsx
+++ index.tsx
@@ -1,7 +1,18 @@
 // src/index.tsx
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://dev.vm:18766", "http://analysis:8766"], // 允许前端所在的域名
+    methods: ["GET", "POST"], // 允许的请求方法
+    credentials: true, // 处理认证或 cookie 时启用
+  })
+);
+
+app.use(express.json());
 
 // 定义一个简单的路由
 app.get("/", (req, res) => {
@@ -9,13 +20,13 @@ app.get("/", (req, res) => {
 });
 
 // mock api
-app.get('/mock', (req, res) => {
+app.get("/mock", (req, res) => {
   res.json({
     success: true,
     data: [
-      { id: 1, code: '123_000', value: 101 },
-      { id: 2, code: 'qwe_SSS', value: 202 },
-    ]
+      { id: 1, code: "123_000", value: 101 },
+      { id: 2, code: "qwe_SSS", value: 202 },
+    ],
   });
 });
 
